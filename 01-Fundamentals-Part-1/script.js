3a2673b44198e21744ee5b6356762793079b5d26
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-09 08:41:45 +0200

    End of the lesson, starting the assignment

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,27 @@
 let js = "amazing";
-if (js == "amazing") alert("is very fun!");
+if (js == "amazing") console.log("is very fun!");
 console.log(40 + 8);
+
+console.log("Marco");
+console.log(23);
+
+let nome = "Marco";
+console.log(nome);
+console.log(nome);
+console.log(nome);
+
+let marco_sofiya = "MS";
+
+let Person = "Marco"; //legal but not usual
+
+let PI = 3.1415; //the upper cse indicate a constant
+
+let myFirstJob = "waiter";
+let myCurrentJob = "programmer";
+
+//Is better than
+
+let job1 = "Waiter";
+let job2 = "Programmer"
+
+console.log(myCurrentJob);
