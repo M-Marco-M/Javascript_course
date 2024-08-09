57965287b79e63cb0767b23763599ae6aa625af8
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-09 09:13:36 +0200

    Lezione tipi javascript

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 if (js == "amazing") console.log("is very fun!");
 console.log(40 + 8);
@@ -10,6 +11,7 @@ console.log(nome);
 console.log(nome);
 console.log(nome);
 
+//Variable name convention
 let marco_sofiya = "MS";
 
 let Person = "Marco"; //legal but not usual
@@ -25,3 +27,32 @@ let job1 = "Waiter";
 let job2 = "Programmer"
 
 console.log(myCurrentJob);
+*/
+
+//Javascript types
+console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof "Prova");
+// console.log(typeof 23);
+console.log(typeof javascriptIsFun);
+
+// In Javascript è il valore a determinare il tipo e non la variabile, 
+// il cui tipo invece si adatta a quello del valore.
+// --Dynamic typing
+
+//Adesso il tipo è cambiato in stringa
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year); //il valore è undefined
+console.log(typeof year); //il tipo è undefined
+
+year = 1991;
+console.log(typeof year); //è number
+
+console.log(typeof null); //stamperà bject, ma è un bug di javascript, dovrebbe stampare null, il suo tipo è null;
+
