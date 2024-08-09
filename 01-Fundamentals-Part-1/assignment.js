5d52617312a4c36e22bfe9e568945545d1da26ec
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-09 08:49:29 +0200

    End of the assignment

---
+++ 01-Fundamentals-Part-1/assignment.js
@@ -0,0 +1,6 @@
+//Exercise
+let country = "Italy";
+let continent = "Europe";
+let population = "60M";
+
+console.log("Country = ", country, ", Continent = ", continent, ", Population = ", population);
