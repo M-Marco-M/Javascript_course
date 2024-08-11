e36d8ab9982b3035dc1223fae53cf503c25ace0c
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-11 10:18:49 +0200

    Svolge secondo esercizio
    L'esercizio prevedeva l'utilizzo del costrutto if - else

--- 01-Fundamentals-Part-1/exercise.js
+++ 01-Fundamentals-Part-1/exercise.js
@@ -26,3 +26,10 @@ BMIJohn = massJohn / (heightJohn * heightJohn);
 markHigherBMI = BMIMark > BMIJohn;
 
 console.log("BMI Mark:" + BMIMark,"BMI John:" + BMIJohn, "Mark BMIIsHigher: " + markHigherBMI);
+
+//Esercizio 2
+if (markHigherBMI) {
+    console.log(`Il BMI di Mark (${BMIMark}) è maggiore di quello di John (${BMIJohn})`);
+} else {
+    console.log(`Il BMI di John (${BMIJohn}) è maggiore di quello di Mark (${BMIMark})`);
+}
