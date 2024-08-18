22c11ff14f990286da0a6a101d15accd392141a8
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-18 07:15:14 +0200

    Lezione 50: node e live server

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+//Lezione 50: configurazione prettier
+const x = 23;
+
+const y = 'prova stringa';
+
+const z = 'string';
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+console.log('prova');
+
+console.log('Pazzesco');
+
+//Lezione 51: installazione di node (tramite nvm) e di live-server
