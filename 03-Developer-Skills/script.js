5a91f3d1e9a20dbd99393245e629153b9c1e7b25
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-19 09:02:23 +0200

    Termina lezione 60 e challenge

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -2,17 +2,17 @@
 'use strict';
 
 //Lezione 55: configurazione prettier
-const x = 23;
+// const x = 23;
 
-const y = 'prova stringa';
+// const y = 'prova stringa';
 
-const z = 'string';
+// const z = 'string';
 
-const calcAge = (birthYear) => 2037 - birthYear;
+// const calcAge = (birthYear) => 2037 - birthYear;
 
-console.log('prova');
+// console.log('prova');
 
-console.log('Pazzesco');
+// console.log('Pazzesco');
 
 //Lezione 56: installazione di node (tramite nvm) e di live-server
 
@@ -99,23 +99,89 @@ const temperatures2 = [-3, 2, 6, 1, 'error', -9, -13, -17, -15, -14, -9, -5];
 //La funzione adesso deve ricevere due array
 //La soluzione è  unire i due array
 
-const calcTempAmplitudeNew = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
-  let max = temps[0];
-  let min = temps[0];
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log('Max: ' + max);
-  console.log('Min: ' + min);
-  return max - min;
-};
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log('Max: ' + max);
+//   console.log('Min: ' + min);
+//   return max - min;
+// };
 
-console.log(calcTempAmplitudeNew(temperatures, temperatures2));
+// console.log(calcTempAmplitudeNew(temperatures, temperatures2));
 
 //Lezione 60: i bug
-//ddd
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temperature',
+//     unit: 'celsius',
+//     //FIX
+//     value: Number(prompt('Degrees celsius')),
+//     // value: prompt('Degrees celsius'),
+//   };
+//   // console.log(measurement);
+//   console.table(measurement);
+//   console.log(measurement.value);
+//   // console.warn(measurement.value);
+//   // console.error(measurement.value);
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+//1)IDENTIFY: La funzione mostra 10273, non il risultato che desideriamo
+//2)WHY: L'espressione kelvin fa un concat di stringhe
+//3)CAUSE: "value" è una stringa -> prommpt immagazzina stringhe
+//4)SOLUTION: convertire value a number
+// console.log(measureKelvin());
+
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+//   let max = temps[i];
+//   let min = temps[i];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log('Max: ' + max);
+//   console.log('Min: ' + min);
+//   return max - min;
+// };
+
+// console.log(calcTempAmplitudeBug(temperatures, temperatures2));
+
+//Challenge
+
+//Dato un array di temperature max previste pe r i giorni a seguire
+//stampare una stringa di presentazione delle previsioni del tempo per ogni giorno
+
+//Esempio: [17, 21, 23] stamperà "...17°C fra 1 giorni, 21°C tra 2 giorni, 23°C fra tre giorni"
+
+//Crea una funzione "printForecast" che prende in ingresso un array e stampa la stringa
+
+//- Scorrere l'array con un ciclo
+//- Per ogni giro stampare una stringa che includa anche l'indice e il valore contenuto nella casella con quell'indice
+//- Inserire ognuna delle stringhe in una stringa, concotenandola alle precedenti
+
+const printForecast = function (arr) {
+  let stringToPrint = '';
+  for (let i = 0; i < arr.length; i++) {
+    const currTemp = arr[i];
+    stringToPrint += `... ${currTemp}° fra ${i + 1} giorni `;
+  }
+  return stringToPrint + '...';
+};
+
+console.log(printForecast([27, 24, 26, 31]));
+
+//Problema: concat non fa ciò che mia aspetto. Non funziona come l'append dello stringbuilder di java, va fatta l'assegnazione
