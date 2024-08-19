d4ea4e51f603e2ce92b44ecc092345046557b8b8
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-19 15:19:18 +0200

    Lezione 71: il DOM
    Spiega cosa è il DOM, come è strutturato e a cosa serve

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -6,3 +6,13 @@
 console.log(document.querySelector('.message'));
 //Questo ne stampa il contenuto
 console.log(document.querySelector('.message').textContent);
+
+//Lezione 71: il DOM
+//il DOM (Document Object Model) è una rappresentazione a oggetti del documento HTML, in cui
+//ogni singolo elemento viene rappresentato come un oggetto. Si può tracciare una similitudine con
+//gli oggetti Java, che hanno oggetti "figli" e "padri".
+//Questi oggetti possono essere rappresentati come nodi su un diagramma ad albero:
+//Gli elementi più in basso eridatano da quelli in alto, come se fosse un albero genealogico
+
+//Il DOM è ciò che permette a JavaScript di entrare in relazione con il coumento HTML
+//Il DOM non è parte di JS, ma sono delle librerie scritte in JS messe a disposizione dai browser
