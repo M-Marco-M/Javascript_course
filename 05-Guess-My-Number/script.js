fdf0c42768c1b5435b66474e4d911947d66e858a
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-19 15:08:25 +0200

    Lezione 70: inizio del progetto
    Spiega la selezione degli elementi html in javascript

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+//Lezione 70: Inizio del progetto "Guess my number!"
+
+//Stampa in console l'elemento html identificato dall'id message
+console.log(document.querySelector('.message'));
+//Questo ne stampa il contenuto
+console.log(document.querySelector('.message').textContent);
