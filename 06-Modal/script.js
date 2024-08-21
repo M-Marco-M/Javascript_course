53eac53b313c7027bb4d12e4497e28a20e915a16
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-21 08:32:43 +0200

    Lezione 82: progetto 3
    - Mostra l'uso dei diagrammi di flusso.
    - Introduce getElementById
    - Mostra un'impostazione di stesura di
    un buon codice, con commenti e sezioni.

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -60,8 +60,7 @@ overlay.addEventListener('click', closeModal);
 //Questa funzione prende in ingresso un parametro, javascript passa automaticamente
 //l'oggetto dell'evento, cioè il primo oggetto che trova all'accadere dell'evento
 document.addEventListener('keydown', function (e) {
+  //Ogni volta che un evento accade javascript crea un oggetto che conserva tutte le informazioni che lo riguardano
   // console.log(e); //dentro e c'è il parametro "key", cioè i tasto premuto
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
 });
-
-//Ogni volta che un evento accade javascript crea un oggetto che conserva tutte le informazioni che lo riguardano
--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,14 @@
 'use strict';
+//Lezione 82: Progetto 3
+
+//Selezione elementi del DOM
+//Con El nel nome rendiamo più facile ricordarci che è un elemnto del DOM
+const score0El = document.querySelector('#score--0'); //# indica un ID
+const score1El = document.getElementById('score--1'); //per l'id può essere usato anche getElementById, non c'è bisogno di scrivere #. la ricerca dell'elemnto è più veloce (probabilmente perchè sa già di dover contollare solo gli id)
+const diceEl = document.querySelector('.dice');
+//Aggiungo la classe "hidden" al file css
+
+//Condizione di partenza
+diceEl.classList.add('hidden');
+score0El.textContent = 0;
+score1El.textContent = 0;
