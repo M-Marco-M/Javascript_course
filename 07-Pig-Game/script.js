16c05ddaf4a1293111e2479e408ef8d73af56c65
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-08-22 08:43:33 +0200

    Lezione 84: cambio del giocatore
    Sviluppa la logica del cambio del giocatore, utilizza
    una selezione dinamica del giocatore attivo

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -4,7 +4,7 @@
 //per tutto lo script. Ogni lezione sarà recuperabile tramite la storia di git
 //Lezione 82: Progetto 3
 //Lezione 83: sviluppo della logica - il tiro del dado
-
+//Lezione 84: il cambio del giocatore
 //-Selezione elementi del DOM
 //Con El nel nome rendiamo più facile ricordarci che è un elemnto del DOM
 const score0El = document.querySelector('#score--0'); //# indica un ID
@@ -15,6 +15,8 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0 = document.getElementById('current--0');
 const current1 = document.getElementById('current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 //Aggiungo la classe "hidden" al file css
 
 //-Condizione di partenza
@@ -24,6 +26,8 @@ score1El.textContent = 0;
 
 //-Dichiarazione variabili globali
 let currentScore = 0;
+let scores = [0, 1];
+let activePlayer = 0;
 
 //-Funzioni
 //--Funzione lancio dado
@@ -37,10 +41,22 @@ const rollDice = function () {
   if (dice !== 1) {
     //Somma al punteggio attuale
     currentScore += dice;
-    score0El.textContent = score0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    console.log(currentScore);
   } else {
     //Passa all'altro giocatore
     currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.toggle('player--active');
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.toggle('player--active');
+    console.log(activePlayer);
   }
 };
 
