2c278cffcf54080a5ca0fb274facf0cbbaa468e9
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-09-20 10:57:52 +0200

    Lezione 123: pratica con le stringhe
    Manipolazione avanzata di una stringa

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -915,3 +915,51 @@ const planesInLine = function (numPlanes) {
 };
 
 console.log(planesInLine(5));
+
+//Lezione 123: pratica con le stringhe
+
+//A partire da qeusta stringa data ottenere quella sotto
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const flightsArray = flights
+  .replaceAll('_', ' ')
+  .replaceAll('+', ')\n.')
+  .replaceAll(':', 'h')
+  .replaceAll('fao93766109', 'FAO')
+  .replaceAll('txl2133758440', 'TXL')
+  .replaceAll('bru0943384722', 'BRU')
+  .replaceAll('hel7439299980', 'HEL')
+  .replaceAll('lis2323639855', 'LIS')
+  .replaceAll(';', ';,')
+  .replaceAll('Delayed', ' ❌ Delayed')
+  .split(';');
+
+let k = 0;
+
+const newFlightsArray = [];
+for (const string of flightsArray) {
+  if (k % 2 === 0 && k % 3 !== 0) {
+    newFlightsArray.push(string.replace(',', ' to '));
+  } else if (k % 3 === 0) {
+    newFlightsArray.push(string.replace(',', ' ('));
+  } else {
+    newFlightsArray.push(string.replace(',', ' from '));
+  }
+  k++;
+}
+
+const finalFlightsArray = newFlightsArray.join('').split('.');
+console.log(finalFlightsArray);
+
+const finalFlights = [];
+for (const element of finalFlightsArray) {
+  finalFlights.push(element.padStart(' ', 1000));
+}
+
+console.log(finalFlights.join(''));
