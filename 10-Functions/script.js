5b8add1f6b97e4c68ef285bc6dca8be5eb43f6c8
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-09-23 15:38:07 +0200

    Lezione 129: Parametri di default

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,29 @@
 'use strict';
+//Lezione 129: Parametri di default
+const bookings = [];
+//In JS 6 si può assegnare direttamente nella firma
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 / numPassengers
+) {
+  //Per assegnare un valore standard in JS5
+  //numPassengers = numPassengers || 1;
+
+  //Creo l'oggetto
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('B229', 10, 78);
+createBooking('A127');
+//Se passiamo un solo valore il resto è undefined
+
+//Adesso per far si che un prametro assuma il valore di default, assegnando però il successivo,
+//bisognerebbe passare undefined
+createBooking('A228', undefined, 37);
