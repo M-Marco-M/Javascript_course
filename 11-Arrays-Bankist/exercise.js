74640169b63c6c750f794b3f715286ad34d456e7
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-09-27 14:56:43 +0200

    Esercizio 16: uso dei metodi map, filter e reduce

--- 11-Arrays-Bankist/exercise.js
+++ 11-Arrays-Bankist/exercise.js
@@ -26,3 +26,21 @@ const checkDogs = function (dogsJulia, dogsKate) {
 
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+//Esercizio 16
+//Crea una funzione calcAverageHumanAge che prenda in ingresso un array di età di cani
+//-1) Converte l'età dei cani in anni degli umani secondo la seguente formula:
+//dogAge <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
+//humanAge = 16 + dogAge * 4
+//-2) Esclude tutti i cani che hanno età umana < 18
+//-3) Calcola l'età media dei cani adulti
+const calcAverageHumanAge = function (dogsAges) {
+  const humanAges = dogsAges.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
+  console.log(humanAges);
+  const adults = humanAges.filter(age => age >= 18);
+  return adults.reduce((avg, age) => avg + age, 0) / adults.length;
+};
+
+console.log(calcAverageHumanAge([3, 5, 2, 12, 7]));
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
